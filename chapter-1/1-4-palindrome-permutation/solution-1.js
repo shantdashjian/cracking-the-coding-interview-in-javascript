a5f428b6add2dcb33a30c52d019d745128760d0a
Shant Dashjian <shantgdashjian@gmail.com>
2024-10-05 15:00:03 +0400

    Add solution for 1.4 Palindrome Permutation question

---
+++ chapter-1/1-4-palindrome-permutation/solution-1.js
@@ -0,0 +1,29 @@
+function palindromePermutation(string) {
+    const map = frequencyMap(string)
+    let foundOdd = false
+    for (let frequency of Object.values(map)) {
+        if (frequency % 2 !== 0) {
+            if (foundOdd) {
+                return false
+            } 
+            foundOdd = true
+        }
+    }
+    return true
+}
+
+function frequencyMap(string) {
+    const map = {}
+    for (let i = 0; i < string.length; i++) {
+        const key = string[i].toLowerCase()
+        if (key !== ' ') {
+            map[key] = key in map ? map[key] + 1 : 1
+        }
+    }
+    return map
+}
+
+console.log(palindromePermutation('Tact Coa'))
+console.log(palindromePermutation('Tact Coaa'))
+
+// O(n) time | O(1) space
