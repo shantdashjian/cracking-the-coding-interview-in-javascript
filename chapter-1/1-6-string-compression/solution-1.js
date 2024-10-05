c2051b82c58bc1e4b61879b51e68810f416b4a42
Shant Dashjian <shantgdashjian@gmail.com>
2024-10-05 16:53:37 +0400

    Add solution for 1.6 String Compression question

---
+++ chapter-1/1-6-string-compression/solution-1.js
@@ -0,0 +1,21 @@
+function stringCompression(string) {
+    let compressed = []
+    let count = 0
+    for (let i = 0; i < string.length; i++) {
+        count += 1
+        if (i === string.length - 1 || string[i] !== string[i+1]) {
+            compressed.push(string[i] + count)
+            count = 0
+        }
+    }
+    compressed = compressed.join('')
+    if (compressed.length >= string.length) {
+        return string
+    }
+    return compressed
+}
+
+console.log(stringCompression('aabcccccaaa'))
+console.log(stringCompression('abc'))
+
+// O(n) time | O(n) space
