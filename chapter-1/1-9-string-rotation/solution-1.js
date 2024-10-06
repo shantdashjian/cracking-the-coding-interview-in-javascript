e02b4759f50aac771673e1109e933549f40bb20b
Shant Dashjian <shantgdashjian@gmail.com>
2024-10-06 10:54:32 +0400

    Add solution for 1.9 String Rotation question

---
+++ chapter-1/1-9-string-rotation/solution-1.js
@@ -0,0 +1,19 @@
+function stringRotation(s1, s2) {
+    return isSubstring(s2, s1 + s1)
+}
+
+function isSubstring(s1, s2) {
+    let s2Start = 0
+    while(s2Start + s1.length < s2.length) {
+        if (s1 === s2.substring(s2Start, s2Start + s1.length)) {
+            return true
+        }
+        s2Start += 1
+    }
+    return false
+}
+
+console.log(stringRotation('erbottlewat', 'waterbottle'))
+console.log(stringRotation('erbottlewat', 'waterbottls'))
+
+// O(a x b) time | O(a + b) space
