8afad7a97a5b9298ec36cf3e9438821356f683f9
Shant Dashjian <shantgdashjian@gmail.com>
2024-10-12 21:25:48 +0400

    Add solution for 2.6 Palindrome question

---
+++ chapter-2/2-6-palindrome/solution-1.js
@@ -0,0 +1,67 @@
+class LinkedList {
+    constructor() {
+        this.head = null
+        this.tail = null
+    }
+
+    append(value) {
+        const node = new Node(value)
+        if (this.tail === null) {
+            this.head = node
+            this.tail = node
+        } else {
+            this.tail.next = node
+            this.tail = node
+        }
+    }
+
+    print() {
+        let list = ''
+        let pointer = this.head
+        if (pointer === null) {
+            list = 'null'
+        } else {
+            do {
+                list += pointer.value + ' -> '
+                pointer = pointer.next
+            } while (pointer !== null)
+            list += 'null'
+        }
+        console.log(list)
+    }
+}
+
+class Node {
+    constructor(value) {
+        this.value = value
+        this.next = null
+    }
+}
+
+function palindrome(head) {
+    const content = []
+    while (head !== null) {
+        content.push(head.value)
+        head = head.next
+    }
+    for (let i = 0; i < content.length / 2; i++) {
+        if (content[i] !== content[content.length - 1 - i]) {
+            return false
+        }
+    }
+    return true
+}
+
+// Arrange
+const linkedList = new LinkedList()
+linkedList.append('a')
+linkedList.append('b')
+linkedList.append('a')
+
+// Act
+const result = palindrome(linkedList.head)
+
+// Assert
+console.log(result)
+
+// O(n) time | O(n) space
